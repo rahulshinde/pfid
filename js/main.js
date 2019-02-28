10f9a137c90450498788e2f690fc6b67e3269cb3
Rahul S. Shinde <rshinde@mica.edu>
2019-02-28 15:54:02 -0500

    adding in layout for 2 new sections

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
   Site.body = $('body');
   Site.window_height = $(window).height();
   Site.window_width = $(window).width();
+
+  $('.process_section_heading').on('click', function(){
+    $(this).parent().toggleClass('open');
+  })
 })
